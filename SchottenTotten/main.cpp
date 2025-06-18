732f969bd2e2a61dc002aa151f57fd2ef78d0091
aeliggg <aelig.hamadou@gmail.com>
2025-06-18 15:54:29 +0200

    JE VEUX PULL

--- SchottenTotten/main.cpp
+++ SchottenTotten/main.cpp
@@ -14,8 +14,7 @@
 // === Choix du mode de jeu avec flèches HAUT / BAS ===
 int AfficheChoixModeJeuNavigable() {
     int choix = 0; // 0 = PvP, 1 = PvIA
-    const std::string modes[2] = { "Joueur vs Joueur", "Joueur vs IA" };
-
+    const std::string modes[3] = { "Joueur vs Joueur", "Joueur vs IA"};
     while (true) {
         clearConsole();
         std::cout << "=== Choisissez le mode de jeu ===\n\n";
@@ -46,6 +45,43 @@ int AfficheChoixModeJeuNavigable() {
         }
     }
 }
+
+int AfficheChoixTypeJeuNavigable() {
+    int choix = 0; // Index du mode sélectionné (0 à 2)
+    const int nbModes = 3;
+    const std::string modes[nbModes] = { "Normal", "Expert", "Tactique" };
+
+    while (true) {
+        clearConsole();
+        std::cout << "=== Choisissez le mode de jeu ===\n\n";
+
+        for (int i = 0; i < nbModes; ++i) {
+            if (i == choix) {
+                std::cout << " > [" << modes[i] << "]\n";
+            }
+            else {
+                std::cout << "   " << modes[i] << "\n";
+            }
+        }
+
+        std::cout << u8"\nUtilisez les flèches HAUT/BAS pour naviguer, Entrée pour valider." << std::endl;
+
+        int key = _getch();
+        if (key == 224) {
+            key = _getch();
+            if (key == 72) { // Flèche HAUT
+                choix = (choix - 1 + nbModes) % nbModes;
+            }
+            else if (key == 80) { // Flèche BAS
+                choix = (choix + 1) % nbModes;
+            }
+        }
+        else if (key == 13) { // Entrée
+            return choix + 1; // 1 = Normal, 2 = Expert, 3 = Tactique
+        }
+    }
+}
+
 int main() {
     std::cout << "Chargement du jeu ..." << std::endl;
     PlaySound(TEXT("Start_sound.wav"), NULL, SND_FILENAME | SND_ASYNC);
@@ -57,7 +93,7 @@ int main() {
 
     std::cout << u8"=== Bienvenue dans le jeu des Bornes ===\n\n";
     int choixMode = AfficheChoixModeJeuNavigable();
-
+    int choixType = AfficheChoixTypeJeuNavigable();
     std::string Nom1, Nom2;
     std::cout << u8"\nJoueur 1, veuillez entrer votre prénom : ";
     std::cin >> Nom1;
@@ -84,13 +120,12 @@ int main() {
         std::vector<Cartes> cartes = partie->getCartes();
         std::cout << "Nombre total de cartes dans la partie : " << cartes.size() << std::endl;
 
-        if (choixMode == 1) {
+        if (choixMode == 1 && choixType==1) {
             veutRejouer = partie->jouer(); // mode normal
         }
-        else {
+        else if (choixMode == 2 && choixType == 1){
             veutRejouer = partie->jouerIA(); // mode IA
         }
-
         delete partie;
         delete joueur1;
         delete joueur2;
